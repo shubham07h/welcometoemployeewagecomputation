b76d60ef21d7f776e912c946bf02119a85bef235
shubham07h <shubham1609sharma@gmail.com>
2022-11-23 21:32:26 +0530

    Reflector:Solving using Switch Case Statement

--- checkemployee.sh
+++ checkemployee.sh
@@ -1,15 +1,17 @@
 isPartTime=4;
 isfullTime=8;
-randomCheck=$((RANDOM%3));
-empRatePerH=20;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
 
-if [ $isfullTime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
-	salary=$((empHrs*empRatePerH));
+case $empCheck in
+	$isfullTime)
+		empHrs=8;
+		;;
+	$ispartTime)
+		empHrs=4;
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+salary=$(($empHrs*$empRatePerHr));
