a5627a2a429088d5766a0ca951db5e8f25cde292
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 20:26:10 +0530

    Add UC7_WorkHour

--- wagecal.sh
+++ wagecal.sh
@@ -19,11 +19,11 @@ totalSalary=0;
 totalWorkingHour=0;
 day=1;
 
-while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
 do
 	wHour=$(calculateWorkingHour $((RANDOM%3)));
 	totalWorkingHour=$(($totalWorkingHour + $wHour));
-	if [ $totalWorkingHour -gt 100 ]
+	if [ $totalWorkingHour -gt 40 ]
 	then
 		totalWorkingHour=$(($totalWorkingHour - $wHour));
 		break;
