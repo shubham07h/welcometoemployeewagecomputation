eb51f627fb09077edb6a9a4f69afb8b18d27e249
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 20:19:57 +0530

    Add UC6_Working Hour

--- wagecal.sh
+++ wagecal.sh
@@ -1,34 +1,34 @@
+function calculateWorkingHour() {
+	case $1 in
+		0)
+			workingHour=0;
+			;;
+
+		1)
+			workingHour=8;
+			;;
+		2)
+			workingHour=4;
+			;;
+	esac;
+	echo $workingHour;
+}
+
 perHourSalary=20;
-workingHour=0;
 totalSalary=0;
 totalWorkingHour=0;
 day=1;
-while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
-	isPresent=$((RANDOM%3));
-	case $isPresent in
-		0)
-		#echo "Employee is absent";
-		workingHour=0;
-		;;
-
-		1)
-		#echo "Employee is present";
-		workingHour=8;
-		;;
-
-		2)
-		#echo "Employee is working as part time";
-		workingHour=4;
-		;;
-	esac
-	totalWorkingHour=$(($totalWorkingHour + $workingHour));
-	if [ $totalWorkingHour -gt 40 ]
+	wHour=$(calculateWorkingHour $((RANDOM%3)));
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
+	if [ $totalWorkingHour -gt 100 ]
 	then
-		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
 		break;
 	fi
-	salary=$(($perHourSalary * $workingHour));
+	salary=$(($perHourSalary * $wHour));
 	totalSalary=$(($totalSalary + $salary));
 	((day++));
 done
